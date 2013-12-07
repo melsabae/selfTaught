0eb47f370d0a99fb73c397dc4d5e96f84b3b77d9
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2013-12-07 12:08:26 -0600

    had continue in the wrong place, added a bool to check if array sorting should go on

--- c/ch8/ch8_Dc_include.c
+++ c/ch8/ch8_Dc_include.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <assert.h>
 
 /* once i figure out how to evaluate performance of these algorithms
@@ -9,10 +10,10 @@
 /* spills array to screen, in 5 columns, tab delimited
  * things get weird, however the structure is mostly correct,
  * 	when using long numbers */
-void printArray(long long *array, unsigned short arrayLength)
+void printArray(long long *array, unsigned short arrayLen)
 {
 	printf("array[*] = {\n");
-	for (unsigned register short i = 0; i < arrayLength; i++, array++) {
+	for (unsigned register short i = 0; i < arrayLen; i++, array++) {
 
 		/* 1 is added to i in the if condition
 		 *	so that natural counting is what happens when
@@ -30,53 +31,58 @@ void printArray(long long *array, unsigned short arrayLength)
 // im leaving this one in here for comparison purposes
 /* selection sort implementation, using nested for-loops
  * brings smallest numbers to the top */
-void arraySelectionSort(long long *array, unsigned short arrayLength)
+void arraySelectionSort(long long *array, unsigned short arrayLen)
 {
 	long long temp;
 
-	/* i and arrayPosition are used differently,
-	 *	arrayPosition moves up to the next element in the array
+	/* i and arrayIndex are used differently,
+	 *	arrayIndex moves up to the next element in the array
 	 *	and starts each iteration from that point
 	 *	in each iteration of the outer loop
 	 * i absorbs that value, moving through each element
 	 *	incrementally on the inner loop */
-	for (unsigned register short arrayPosition = 0;
-			arrayPosition < arrayLength; arrayPosition++)
-		for (unsigned register short i = arrayPosition;
-				i < arrayLength; i++)
-			if (array[i] < array[arrayPosition]) {
+	for (unsigned register short arrayIndex = 0;
+			arrayIndex < arrayLen; arrayIndex++)
+		for (unsigned register short i = arrayIndex;
+				i < arrayLen; i++)
+			if (array[i] < array[arrayIndex]) {
 				temp = array[i];
-				array[i] = array[arrayPosition];
-				array[arrayPosition] = temp;
+				array[i] = array[arrayIndex];
+				array[arrayIndex] = temp;
 			}
 
-	assert (array[0] < array[arrayLength - 1]);
+	assert (array[0] < array[arrayLen - 1]);
 }
 
 /* slightly modified for quicker execution */
-void arraySelectionSort2(long long *array, unsigned short arrayLength)
+void arraySelectionSort2(long long *array, unsigned short arrayLen)
 {
 	long long temp;
-
-	for (unsigned register short arrayPosition = 0;	arrayPosition < arrayLength; arrayPosition++) {
-		for (unsigned register short i = arrayPosition;	i < arrayLength; i++) {
-			if (array[i] < array[arrayPosition]) {
+	bool modified = false;
+	
+	for (unsigned register short arrayIndex = 0; arrayIndex < arrayLen; arrayIndex++) {
+		for (unsigned register short i = arrayIndex; i < arrayLen; i++) {
+			if (array[i] < array[arrayIndex]) {
 				temp = array[i];
-				array[i] = array[arrayPosition];
-				array[arrayPosition] = temp;
+				array[i] = array[arrayIndex];
+				array[arrayIndex] = temp;
+				modified = true;
 			}
-
+			
 			/* after finding a smaller value, no need to
 			 * go through the rest of the array
 			 * at least according to this logic */
 
 			 /* should be noted that SO FAR this works
 			  * and has passed many times flawlessly */
-			continue;
 		}
-	}
 
-	assert (array[0] < array[arrayLength - 1]);
+		if (modified)
+			continue;
+		else
+			printf("%d\n", arrayIndex);
+			break;
+	}
 }
 
 /* the description of bubblesort appeared to be the same as the
@@ -90,19 +96,19 @@ void arraySelectionSort2(long long *array, unsigned short arrayLength)
  *
  * i will likely not implement a true bubble sort, the algorithm
  * suggests inefficiency in terms of IO */
-void arrayBubbleSort(long long *array, unsigned short arrayLength)
+void arrayBubbleSort(long long *array, unsigned short arrayLen)
 {
 	long long temp; 
 
-	for (unsigned register short arrayPosition = 0;
-			arrayPosition < arrayLength; arrayPosition++)
-		for (unsigned register short i = arrayPosition;
-				i < arrayLength; i++, arrayLength--)
-			if (array[i] > array[arrayPosition]) {
-				temp = array[arrayPosition];
-				array[arrayPosition] = array[i];
+	for (unsigned register short arrayIndex = 0;
+			arrayIndex < arrayLen; arrayIndex++)
+		for (unsigned register short i = arrayIndex;
+				i < arrayLen; i++, arrayLen--)
+			if (array[i] > array[arrayIndex]) {
+				temp = array[arrayIndex];
+				array[arrayIndex] = array[i];
 				array[i] = temp;
 			}
 	
-	assert (array[0] > array[arrayLength - 1]);
+	assert (array[0] > array[arrayLen - 1]);
 }
