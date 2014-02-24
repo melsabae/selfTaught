d530df03b72b1ad2fe4d0999144f42de2c33c45e
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2014-02-24 16:37:02 -0600

    updated contacts script for some portability

--- scripts/updateContacts.sh
+++ scripts/updateContacts.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-cd ~/contactwork/
-
 TRKSERV=//rzpc
 REMOTE=//rzpc/trk
 FAIL=`smbclient -NL $TRKSERV | head -1`
+DIR=/home/saba/contactwork
+OUTFILE=$DIR/contacts.csv
+COPYFILE=/mnt/Share/contacts.csv
+ADTFILE=$DIR/INVUSER.ADT
 
 case $FAIL in
 	*failed*) echo `date` "with error:"  $FAIL >> ~/contactwork/faillog.txt; exit;;
 esac
 
+rm $DIR/INVUSER.ADT
+
 smbclient -N $REMOTE -c "cd CO0002; prompt; get INVUSER.ADT; exit;"
 
-echo 'STOCK,SOURCE,CONTACT' > ~/contactwork/contacts.csv
-strings ~/contactwork/INVUSER.ADT | grep -P ^[a-lA-L][0-9]\{3\}\ .* | cut -c1-68 | grep -vP ^[a-lA-L][0-9]\{3\}\ \{3,4\}[\w\ ]\{1,30\}$ | grep -vP ^.\{38\}[^a-zA-Z]*$ | sed -e 's_\(.\{8\}\)\(.\{30\}\)\(.*\)_"\1","\2","\3"_g' -e 's_ \{2,\}__g' | sort >> ~/contactwork/contacts.csv
-cp ~/contactwork/contacts.csv /mnt/Share/contacts.csv
-chmod 644 /mnt/Share/contacts.csv
+echo 'STOCK,SOURCE,CONTACT' > $OUTFILE
+strings $ADTFILE | grep -P ^[a-lA-L][0-9]\{3\}\ .* | cut -c1-68 | grep -vP ^[a-lA-L][0-9]\{3\}\ \{3,4\}[\w\ ]\{1,30\}$ | grep -vP ^.\{38\}[^a-zA-Z]*$ | sed -e 's_\(.\{8\}\)\(.\{30\}\)\(.*\)_"\1","\2","\3"_g' -e 's_ \{2,\}__g' | sort >> $OUTFILE
+cp $OUTFILE $COPYFILE
+chmod 644 $COPYFILE
 exit 0
