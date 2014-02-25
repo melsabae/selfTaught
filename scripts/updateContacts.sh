23cac633fc63ac6288334709fa54368c49839534
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2014-02-25 08:51:47 -0600

    oversight in script caused file to be downloaded to parent dir, fixed in smbclient args

--- scripts/updateContacts.sh
+++ scripts/updateContacts.sh
@@ -14,7 +14,7 @@ esac
 
 rm $DIR/INVUSER.ADT
 
-smbclient -N $REMOTE -c "cd CO0002; prompt; get INVUSER.ADT; exit;"
+smbclient -N $REMOTE -c "cd CO0002; lcd $DIR; prompt; get INVUSER.ADT; exit;"
 
 echo 'STOCK,SOURCE,CONTACT' > $OUTFILE
 strings $ADTFILE | grep -P ^[a-lA-L][0-9]\{3\}\ .* | cut -c1-68 | grep -vP ^[a-lA-L][0-9]\{3\}\ \{3,4\}[\w\ ]\{1,30\}$ | grep -vP ^.\{38\}[^a-zA-Z]*$ | sed -e 's_\(.\{8\}\)\(.\{30\}\)\(.*\)_"\1","\2","\3"_g' -e 's_ \{2,\}__g' | sort >> $OUTFILE
