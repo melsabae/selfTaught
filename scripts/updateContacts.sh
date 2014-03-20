356ba0550066ec3a24824a0e848fce50434785c4
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2014-03-20 14:17:32 -0500

    changed variable names to avoid enviro conflicts, executes twice as fast, error reporting works better, logic changes

--- scripts/updateContacts.sh
+++ scripts/updateContacts.sh
@@ -1,49 +1,70 @@
+# script that pulls in tracker's adt table file and pulls out contacts # since the database engine doesn't offer anything other than GUI, had to pull straight out of the file using strings
+# quick-n-dirty but the output has not failed _YET_
+
 #!/bin/bash
 # include stdsarcasm.h
 
-DIR=/home/saba/contactwork
-TRKSERV=//rzpc
-FAIL=`smbclient -NL $TRKSERV | head -1`
+# not normally what i would do but it cleans up some other parts and runs leaner
+cd ~/contactwork
+
+trk_serv=//rzpc
+trk_dir_remote=$trk_serv/trk
+fail_log=./faillog.txt
+
+# fail message pulls straight from get operation, combines with an echo to put out smbclient exit code
+	# runs faster than before when done like this
+fail_message=`smbclient -N $trk_dir_remote -c "cd CO0002; prompt; get INVUSER.ADT; exit;"; echo ": smbclient exit code $?"`
 
 # magical all-encompassing error reporting
-case $FAIL in
+case $fail_message in
 	*[Ff][Aa][Ii][Ll][Ee][Dd]*)
-		echo {`date +%a\ %F\ %T`} operation failed with error: \"$FAIL\", error level $? >> $DIR/faillog.txt;
-		exit;;
+		echo {`date +%a\ %F\ %T`} operation failed with error: $fail_message >> $fail_log;
+		exit 1;;
 	*[Ee][Rr][Rr][Oo][Rr]*)
-		echo {`date +%a\ %F\ %T`} operation failed with error: \"$FAIL\", error level $? >> $DIR/faillog.txt;
-		exit;;
+		echo {`date +%a\ %F\ %T`} operation failed with error: $fail_message >> $fail_log;
+		exit 1;;
 esac
 
 # survived.
-REMOTE=//rzpc/trk
-OUTFILE=$DIR/contacts.csv
-COPYFILE=/mnt/Share/contacts.csv
-ADTFILE=$DIR/INVUSER.ADT
-
-smbclient -N $REMOTE -c "cd CO0002; lcd $DIR; prompt; get INVUSER.ADT; exit;"
+	# just in case
+	unset fail_message fail_log trk_dir_remote trk_serv
+contact_outfile=./contacts.csv
+copy_file=/mnt/Share/contacts.csv
+adt_file=./INVUSER.ADT
 
 # blows away file with new copy, because laziness++
-echo 'STOCK,SOURCE,CONTACT' > $OUTFILE
+	# column headings, double quoted for consistency with later entries
+echo '"STOCK","SOURCE","CONTACT"' > $contact_outfile
 
-# it gets gnarly here
-# AKA: the actual work
-
-# we're using strings because there's no shell for the database engine we have to use
-# dirty dirty non-hack
-# then a cut, since the database engine stores unused space as whitespace, this makes the next part simpler
-# grep away anything not having a purchase source and/or a contact
-# comma delimit && wrap each token in double quotes, then remove extra whitespace
-# sort then dump output into a .csv file
-strings $ADTFILE \
-	| grep -P ^[a-lA-L][0-9]\{3\}\ .* \
+	# it gets gnarly here
+	# AKA: the actual work
+# 1. we're using strings because there's no shell for the database engine we have to use
+# 2. grep out stock numbers
+# 3. a cut, since the database engine stores unused space as whitespace, this makes the next part simpler
+# 4. grep away anything not having a purchase source and/or a contact
+#    notice the | separating regexes
+# 5. comma delimit && wrap each token in double quotes, then remove extra whitespace
+#    because sometimes purchase source or contacts fields have commas in it, we wrap tokens in double quotes without having to try and remove said commas
+	# database stores stock number as first 8 chars, purchase source as next 30, and contact field as final 30, this is where those magic numbers
+	# come from
+		# sometimes stores whitespace at the end of tokens, we can use field widths later on, or correct it in this script
+#    then remove extra whitespace
+# 6. sort
+# 7. then dump output into a .csv file
+strings $adt_file \
+	| grep -P "^[a-lA-L][0-9]{3} .*" \
 	| cut -c1-68 \
-	| grep -vP ^[a-lA-L][0-9]\{3\}\ \{3,4\}[\w\ ]\{1,30\}$\|^.\{38\}[^a-zA-Z]*$ \
-	| sed -e 's_\(.\{8\}\)\(.\{30\}\)\(.*\)_"\1","\2","\3"_g' -e 's_ \{2,\}__g' \
-	| sort >> $OUTFILE
+	| grep -vP "^[a-lA-L][0-9]{3} {3,4}[\w ]{1,30}$|^.{38}[^a-zA-Z]*$" \
+	| sed -e 's_\(.\{8\}\)\(.\{30\}\)\(.*\)_"\1","\2","\3"_g' -e 's_\ \{2,\}__g' \
+	| sort \
+	>> $contact_outfile
 
-cp $OUTFILE $COPYFILE
+cp $contact_outfile $copy_file
 
 # <srs> so people can't modify output file by accident </srs>
-chmod 644 $COPYFILE
+chmod 644 $copy_file
+
+# just in case
+unset trk_serv trk_dir_remote contact_outfile copy_file adt_file
+
 exit 0
