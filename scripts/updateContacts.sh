5b65252e1f05f458592af15f5a6e6b25c6d994e4
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2014-03-24 11:53:14 -0500

    simplified escaped regexes

--- scripts/updateContacts.sh
+++ scripts/updateContacts.sh
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 # script that pulls in tracker's adt table file and pulls out contacts # since the database engine doesn't offer anything other than GUI, had to pull straight out of the file using strings
-=======
-# script that pulls in tracker's adt table file and pulls out contacts
-# since the database engine doesn't offer anything other than GUI, had to pull straight out of the file using strings
->>>>>>> 37711b4b2146d0d3ae79c9137e3aec1f8d8ff707
 # quick-n-dirty but the output has not failed _YET_
 
 #!/bin/bash
@@ -17,10 +12,7 @@ trk_dir_remote=$trk_serv/trk
 fail_log=./faillog.txt
 
 # fail message pulls straight from get operation, combines with an echo to put out smbclient exit code
-<<<<<<< HEAD
 	# runs faster than before when done like this
-=======
->>>>>>> 37711b4b2146d0d3ae79c9137e3aec1f8d8ff707
 fail_message=`smbclient -N $trk_dir_remote -c "cd CO0002; prompt; get INVUSER.ADT; exit;"; echo ": smbclient exit code $?"`
 
 # magical all-encompassing error reporting
@@ -42,16 +34,11 @@ adt_file=./INVUSER.ADT
 
 # blows away file with new copy, because laziness++
 	# column headings, double quoted for consistency with later entries
-<<<<<<< HEAD
-=======
-	# if in the event i use output in other applications
->>>>>>> 37711b4b2146d0d3ae79c9137e3aec1f8d8ff707
 echo '"STOCK","SOURCE","CONTACT"' > $contact_outfile
 
 	# it gets gnarly here
 	# AKA: the actual work
 # 1. we're using strings because there's no shell for the database engine we have to use
-<<<<<<< HEAD
 # 2. grep out stock numbers
 # 3. a cut, since the database engine stores unused space as whitespace, this makes the next part simpler
 # 4. grep away anything not having a purchase source and/or a contact
@@ -71,23 +58,6 @@ strings $adt_file \
 	| sed -e 's_\(.\{8\}\)\(.\{30\}\)\(.*\)_"\1","\2","\3"_g' -e 's_\ \{2,\}__g' \
 	| sort \
 	>> $contact_outfile
-=======
-#     probably unsanitary
-# 2. grep out all matching new stock numbers
-# 3. a cut, since the database engine stores unused space as whitespace, this makes the next part simpler
-# 4. grep away anything not having a purchase source and/or a contact
-#    notice the \| separating regexes
-# 5. comma delimit && wrap each token in double quotes, then remove extra whitespace
-#    because sometimes purchase source or contacts fields have commas in it, we wrap tokens in double quotes to properly delimit them without having to try and remove said commas
-#    then remove extra whitespace
-# 6. sort then dump output into a .csv file
-strings $adt_file \
-	| grep -P ^[a-lA-L][0-9]\{3\}\ .* \
-	| cut -c1-68 \
-	| grep -vP ^[a-lA-L][0-9]\{3\}\ \{3,4\}[\w\ ]\{1,30\}$\|^.\{38\}[^a-zA-Z]*$ \
-	| sed -e 's_\(.\{8\}\)\(.\{30\}\)\(.*\)_"\1","\2","\3"_g' -e 's_ \{2,\}__g' \
-	| sort >> $contact_outfile
->>>>>>> 37711b4b2146d0d3ae79c9137e3aec1f8d8ff707
 
 cp $contact_outfile $copy_file
 
