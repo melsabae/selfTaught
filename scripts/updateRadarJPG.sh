2720b4c4cd03752ef83441bf7c78114e8fc5c36a
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2014-03-29 17:44:14 -0500

    new machine, moved script into actual scripts dir

--- scripts/updateRadarJPG.sh
+++ scripts/updateRadarJPG.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -o ~/images/radar.jpg i.imwx.com/web/radar/us_se_9regradar_plus_usen.jpg
+curl -o ~/Pictures/radar.jpg i.imwx.com/web/radar/us_se_9regradar_plus_usen.jpg
 
 exit 0
