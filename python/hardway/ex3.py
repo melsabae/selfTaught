6b51d9aa74624aef229c506e3a6ae6f1cf3c26ec
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2014-05-10 19:10:38 -0500

    whooooooops

---
+++ python/hardway/ex3.py
@@ -0,0 +1,48 @@
+# This line adds flavor text
+print "I will now count my chickens:"
+
+# This line divides 30 by 6, then adds the result to 25
+print "Hens", 25 + 30 / 6
+
+# This line calculates 75%4 = 3, then removes 3 from 100.
+print "Roosters", 100 - 25 * 3 % 4
+
+# Adds flavor text
+print "Now I will count the eggs:"
+
+# Does the following calcs
+# 3 + 2 + 1 - 5 + 0 - 1 / 4 + 6
+# 3 + 2 + 1 - 5 - 0 + 6
+# 5 + 1 - 5 + 6
+# 6 - 5 + 6
+# 1 + 6
+# 7
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Flavor text
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Calculates both operands to the < sign, then does the comparison
+# Outputs false on it's own line without having to be told to do so
+print 3 + 2 < 5 - 7
+
+# Simple addition
+print "What is 3 + 2?", 3 + 2
+
+# Simple subtraction 
+print "What is 5 - 7?", 5 - 7
+
+# Flavor text
+print "Oh, that's why it's False."
+
+# Flavor text
+print "How about some more."
+
+# Does the boolean expression, 5 > -2?
+print "Is it greater?", 5 > -2
+
+# Does the boolean expression 5 >= -2?
+print "Is it greater or equal?", 5 >= -2
+
+# Does the boolean expression 5 <= 2?
+print "Is it less or equal?", 5 <= 2
