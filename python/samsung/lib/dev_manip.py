ff75b15aaaf93702510ab6d67afd10155ec193af
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2014-05-17 13:13:54 -0500

    initial work done for replacing scripts with python code

--- python/samsung/lib/dev_manip.py
+++ python/samsung/lib/dev_manip.py
@@ -1,22 +1,77 @@
+#!/usr/bin/env python
 import os
 
-# should code logic of these scripts into python
-# instead of relying on external files
-# then replace ExecScript with a new function that runs direct
-# will need to do effective-UID checks for control
+# old code that should be removed once new functionality implemented
+"""
 bt_ctl = "bt_ctl.sh"
 wifi_ctl = "wifi_ctl.sh"
 perf_level = "perf_level.sh"
 kbd_illum = "kbd_illum.sh"
 
 def GetPath( _file ):
-	""" Returns absolute path to script file """
 	return os.path.abspath( './lib/sh/' + _file )
 
-# only runs if file can be found, and then if it is executable per OS
-# _option's default value is empty
+#only runs if file can be found, and then if it is executable per OS
+#_option's default value is empty
 def ExecScript( _file, _option="" ):
-	""" Runs script file with options """
 	if os.path.exists( _file ) and os.access( _file, os.X_OK ):
 		return os.system( "sudo " + _file + " " + _option)
+"""
+
+sysdev = '/sys/devices/'
+samsung = sysdev + 'platform/samsung'
+
+# we use readstring in the event multiple file paths match the name in the searchpath, we can return which one we like based on that file's expected contents if we pass it in as readstring
+def FindFile( filename, searchpath, readstring = "" ):
+	for root, subdirs, files in os.walk( searchpath ):
+		for file in files:
+			if filename == file and readstring == "":
+				return os.path.join( root, file )
+			elif filename == file and readstring != "":
+				tmp = os.path.join( root, file )
+				with open( tmp, 'r' ) as f:
+					if readstring in f.readline():
+						return tmp
+
+# sometimes the values of FindFile's return file will match, so we want to match a parent directory's name instead
+def FindFileStruct( filename, searchpath, dirname ):
+	for root, subdirs, files in os.walk( searchpath ):
+		if dirname in subdirs:
+			return os.path.join( root, dirname, filename )
+
+# these files exist under /sys/devices/platform/samsung on my machine
+
+#the [:-4] removes the name file, the file we are interested in the contents of, not the actual file itself(both radios employ the same filenames), replacing it with state, the name of the file that controls power to these radios
+bt = FindFile( "name", samsung, "samsung-bluetooth" )
+bt = bt[:-4] + "state"
+wlan = FindFile( "name", samsung, "samsung-wlan" )
+wlan = wlan[:-4] + "state"
+print wlan
+
+kbd_bright = FindFile( "brightness", samsung )
+kbd_maxBright = FindFile( "max_brightness", samsung )
+batt_extend = FindFile( "battery_life_extender", samsung )
+perf_level = FindFile( "performance_level", samsung )
+usb_charge = FindFile("usb_charge", samsung )
+
+# these 2 files exist above platform/samsung so we're using a new path to search for them
+# there is a second directory on my machine that contains an interface to manipulate backlight of the screen, but the legal values can reach as low as 0, turning off the screen entirely and you'd need to write in a value of up to 4650 to turn it back on, hence the acpi_video0 that explicitly states which one i want
+# as well as a radio LED that exists in this structure, but we cannot control that directly from file IO, so acpi_video0 is necessity
+mon_bright = FindFileStruct( "brightness", sysdev, "acpi_video0" )
+mon_maxBright = FindFileStruct( "max_brightness", sysdev, "acpi_video0" )
+
+# can we find the ethernet, mouse, vga out, and audio functions?
+
+# this will blow away the file's current contents, as we are currently wanting to do for hardware control
+def WriteValue( device, value ):
+	with open( device, 'w' ) as _file:
+		_file.write( value )
+
+# worked fine :D
+WriteValue( wlan, "0" )
+WriteValue( bt, "1" )
+WriteValue( kbd_bright, "8" )
+WriteValue( batt_extend, "0" )
+WriteValue( perf_level, "normal" )
+WriteValue( usb_charge, "1" )
 
--- python/samsung/samsung_ctl.py
+++ python/samsung/samsung_ctl.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 from lib import gui
 
-if __name__ == "__main__":
-		prog = gui.form(None)
-		prog.quit()
-		prog.mainloop()
+prog = gui.form(None)
+prog.quit()
+prog.mainloop()
 
