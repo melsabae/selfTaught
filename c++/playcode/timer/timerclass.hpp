873f0b3b73ac27bec9c5caf402a2d887abf0a4e1
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2014-10-02 13:44:32 -0500

    DONT USE CURLY BRACES IN HEADER FILES

--- c++/playcode/timer/timerclass.hpp
+++ c++/playcode/timer/timerclass.hpp
@@ -1,21 +1,22 @@
-// do these even work
 #ifndef TIMERCLASS_HPP
 #define TIMERCLASS_HPP
-#endif
+
 
 class Timer
 {
 	uint16_t start, duration;
 
 	public:
-	Timer(){};
-	Timer( uint16_t , uint16_t ){};
-	void Reset(){};
+	Timer();
+	Timer( uint16_t , uint16_t );
+	void Reset();
 
-	void SetDuration( float arg ){}
-	uint16_t GetStart() const {}
-	uint16_t GetDuration() const {}
+	void SetDuration( float arg );
+	uint16_t GetStart const();
+	uint16_t GetDuration const;
 	// float for when i want the elapsed time to resolve to units less than a second, otherwise would be a short
-	float Elapsed() const {}
+	float Elapsed const();
 
 };
+
+#endif
