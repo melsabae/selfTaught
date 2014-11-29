0ecc153317edde068768328581ec9ae73dfd7e3b
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2014-11-29 09:19:50 -0600

    temperatures were not right compared to website, encoding of arguments

--- python/playcode/tfw.py
+++ python/playcode/tfw.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python2.7
-import urllib2, sys
+import urllib2, sys, urllib
 
 default = '36608'
 url = 'http://thefuckingweather.com/?where='
 
 # this will crash depending on inputs
 if len( sys.argv ) > 1:
-    url = url + sys.argv[ 1 ]
+    url = url + urllib.quote_plus( sys.argv[ 1 ])
 else:
     url = url + default
 
@@ -23,13 +23,13 @@ flavor = ''
 for line in html:
     # magic numbers and dark voodoo
     if 'locationDisplaySpan' in line:
-        where = line.strip()[71:-13]
-    if 'temperature' in line:
-        temp = line.strip()[-7:-5] + '?!'
+        where = line.strip()[ 71:-13 ]
+    if 'p class="large"><span class="temperature"' in line:
+        temp = line.strip()[ 54:-48 ] + '?!'
     if 'remark' in line:
-        remark = line.strip()[18:-4]
+        remark = line.strip()[ 18:-4 ]
     if 'flavor' in line:
-        flavor = line.strip()[24:-4]
+        flavor = line.strip()[ 24:-4 ]
 
 print where
 print temp
