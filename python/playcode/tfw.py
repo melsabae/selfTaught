8632d840fc9343fbd4233cf748ceabd29d69f551
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2014-11-29 09:37:33 -0600

    404 page

--- python/playcode/tfw.py
+++ python/playcode/tfw.py
@@ -4,7 +4,7 @@ import urllib2, sys, urllib
 default = '36608'
 url = 'http://thefuckingweather.com/?where='
 
-# this will crash depending on inputs
+# this will crash depending on inputs, definitely if you use a pipe
 if len( sys.argv ) > 1:
     url = url + urllib.quote_plus( sys.argv[ 1 ])
 else:
@@ -21,15 +21,20 @@ remark = ''
 flavor = ''
 
 for line in html:
+    if "FIND THAT SHIT" in line:
+        print "I CAN'T FIND THAT SHIT"
+        sys.exit()
     # magic numbers and dark voodoo
     if 'locationDisplaySpan' in line:
         where = line.strip()[ 71:-13 ]
     if 'p class="large"><span class="temperature"' in line:
-        temp = line.strip()[ 54:-48 ] + '?!'
+        temp = line.strip()[ 54:-48 ]
     if 'remark' in line:
         remark = line.strip()[ 18:-4 ]
     if 'flavor' in line:
         flavor = line.strip()[ 24:-4 ]
+
+temp = temp + u'\u00B0' + '?!'
 
 print where
 print temp
