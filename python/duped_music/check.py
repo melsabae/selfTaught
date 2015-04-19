aa946085570a9a912ee43716ad090d420050ac4e
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2015-04-19 02:28:53 -0500

    found all music on my system, hashed it, then stored the sorted result. used python to check the hashes to find duplicates.

---
+++ python/duped_music/check.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2.7
+
+dupes = open("dupes", "r")
+dupes2 = open("dupes", "r")
+
+# file where duplicates will be recorded with hashes and filenames
+work = open("work_dupes", "w")
+
+# increment, otherwise it would check the same line against itself
+dupes2.readline()
+
+while True:
+    line = dupes.readline()
+    line2= dupes2.readline()
+
+    if not line2:
+        break
+
+    # first 32 chars per line is md5sum
+    if line[0:32] == line2[0:32]:
+        work.write(line)
+        work.write(line2)
+        work.write("\n")
+
+dupes.close()
+dupes2.close()
+work.close()
