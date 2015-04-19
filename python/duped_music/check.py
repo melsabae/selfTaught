2060ea4c4d13f9ba5cb10b67a76d106e32527c2c
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2015-04-19 15:08:48 -0500

    had a brainfart with git, renamed an output file from the python script to something more tab-complete friendly

--- python/duped_music/check.py
+++ python/duped_music/check.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python2.7
 import os
 
-dupes = open(
-        os.path.expanduser("~/dupes"),
-        'r')
-dupes2 = open(
-        os.path.expanduser("~/dupes"),
-        'r')
+dupes = open("summed", 'r')
+dupes2 = open("summed",'r')
 
 # file where duplicates will be recorded with hashes and filenames
-work = open(
-        os.path.expanduser("~/work_dupes"),
-        "w")
+work = open("worked_hashes", "w")
 
 # increment, otherwise it would check the same line against itself
 dupes2.readline()
