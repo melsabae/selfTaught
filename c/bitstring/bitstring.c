9b98078e1e2a68b502740ad343b863305be263e3
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2017-04-07 03:09:43 -0500

    rewrite bitstrings

--- c/bitstring/bitstring.c
+++ c/bitstring/bitstring.c
@@ -1,119 +1,180 @@
 #include "bitstring.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
-char* GenerateBitStringUnsigned(const uint64_t num)
+#define ERROR_OUT(x) { err = x; goto cleanup; }
+
+bs_type_t bitstring_type_unsigned(uint64_t num)
 {
-  static uint8_t size;
-  static char *bitstr;
-  static int8_t bitpos;
+	if(UINT32_MAX < num)
+	{
+		return (BS_U64);
+	}
 
-  // find the minimum number of bits, worst case, to make a bitstring
-  size = ceil(log2(num));
+	if(UINT16_MAX < num)
+	{
+		return (BS_U32);
+	}
 
-  if(pow(2, size) == num)
-  {
-    size++; // 2^n - 1 max value for N bits, add 1 if 2^n is the number
-  }
+	if(UINT8_MAX < num)
+	{
+		return (BS_U16);
+	}
 
-  // based on the exponent in size, find the number of bits
-  if(size > 32)
-  {
-    size = 64;
-  }
-  else if(size > 16)
-  {
-    size = 32;
-  }
-  else if(size > 8)
-  {
-    size = 16;
-  }
-  else
-  {
-    size = 8;
-  }
-
-  bitstr = calloc(1, size + 1); // 1 space for \0, calloc for auto null terminated
-  size --; // decrement for array access
-
-  if(bitstr != NULL)
-  {
-    // build from MSB to LSB
-    for(bitpos = size; bitpos >= 0; bitpos--)
-    {
-      if(num & (1 << bitpos))
-      {
-        bitstr[size - bitpos] = '1';
-      }
-      else
-      {
-        bitstr[size - bitpos] = '0';
-      }
-    }
-  }
-
-  return bitstr;
+	return (BS_U8);
 }
 
-char* GenerateBitStringSigned(const int64_t num)
+bs_type_t bitstring_type_signed(int64_t num)
 {
-  static uint8_t size;
-  static char *bitstr;
-  static int8_t bitpos;
+	bs_param_t param =
+	{
+		.s = num,
+	};
 
-  if(num < 0)
-  {
-    size = ceil(log2(num * -1));
-    size++; // add a bit for sign bit
-  }
-  else
-  {
-    size = ceil(log2(num));
-  }
+	bs_type_t type = bitstring_type_unsigned(param.u);
 
-  if(pow(2, size) == num)
-  {
-    size++; // 2^n - 1 max value for N bits, add 1 if 2^n is the number
-  }
+	switch(type)
+	{
+		case BS_U8:
+			return BS_S8;
+		case BS_U16:
+			return BS_S16;
+		case BS_U32:
+			return BS_S32;
 
-  if(size > 32)
-  {
-    size = 64;
-  }
-  else if(size > 16)
-  {
-    size = 32;
-  }
-  else if(size > 8)
-  {
-    size = 16;
-  }
-  else
-  {
-    size = 8;
-  }
-
-  bitstr = calloc(1, size + 1); // 1 space for \0, calloc for auto null terminated
-  size --; // decrement for array access
-
-  if(bitstr != NULL)
-  {
-    // build from MSB to LSB
-    for(bitpos = size; bitpos >= 0; bitpos--)
-    {
-      if(num & (1 << bitpos))
-      {
-        bitstr[size - bitpos] = '1';
-      }
-      else
-      {
-        bitstr[size - bitpos] = '0';
-      }
-    }
-  }
-
-  return bitstr;
+		case BS_U64:
+			/* intentional fall-through in case statement */
+		default:
+			return BS_S64;
+	}
 }
 
+bs_error_t bitstring_reset(bs_t* bst)
+{
+	if(NULL == bst)
+	{
+		return (BS_NULL_ARG);
+	}
+
+	bst -> num.u = 0;
+	bst -> type = BS_INIT_ME;
+
+	return (BS_ERR_NONE);
+}
+
+bs_error_t bitstring_create(bs_t* bst)
+{
+	return (bitstring_reset(bst));
+}
+
+bs_error_t bitstring_create_alloc(bs_t** bst)
+{
+	bs_t* _bst							= NULL;
+	bs_error_t err					= BS_ERR_NONE;
+
+	if(NULL == bst)					{ ERROR_OUT(BS_NULL_ARG) }
+	if(NULL != (*bst))			{ ERROR_OUT(BS_INVALID_ARG) }
+
+	_bst										= calloc(1, sizeof(bst));
+	if(NULL == _bst)				{ ERROR_OUT(BS_ALLOC_FAIL) }
+
+	err											= bitstring_create(_bst);
+	if(BS_ERR_NONE != err)	{ ERROR_OUT(err) }
+
+	(*bst)									= _bst;
+cleanup:
+
+	// if the user's pointer was not set, free the allocated memory
+	if(NULL == (*bst))
+	{
+		free(_bst);
+	}
+
+	return									(err);
+}
+
+bs_error_t bitstring_make_signed(bs_t* bst, int64_t num)
+{
+	if(NULL == bst)
+	{
+		return (BS_NULL_ARG);
+	}
+
+	bst -> num.s = num;
+	bst -> type = bitstring_type_signed(num);
+
+	return (BS_ERR_NONE);
+}
+
+bs_error_t bitstring_make_unsigned(bs_t* bst, int64_t num)
+{
+	if(NULL == bst)
+	{
+		return (BS_NULL_ARG);
+	}
+
+	bst -> num.u = num;
+	bst -> type = bitstring_type_unsigned(num);
+
+	return (BS_ERR_NONE);
+}
+
+bs_error_t bitstring_serialize(bs_t* bst, void* buf, size_t buf_len)
+{
+	size_t len = 0;
+	size_t i = 0;
+	uint8_t* buffer = NULL;
+
+	if(NULL == bst)
+	{
+		return (BS_NULL_ARG);
+	}
+
+	if(NULL == buf)
+	{
+		return (BS_NULL_ARG);
+	}
+
+	if(BS_INIT_ME == bst -> type)
+	{
+		return (BS_UNINITIALIZED);
+	}
+
+	switch(bst -> type)
+	{
+		case BS_S8:
+			len = 8u;
+			break;
+		case BS_S16:
+			len = 16u;
+			break;
+		case BS_S32:
+			len = 32u;
+			break;
+		case BS_S64:
+			len = 64u;
+			break;
+
+		default:
+			return (BS_INVALID_ARG);
+	}
+
+	if((len + 1) < buf_len)
+	{
+		return (BS_BUF_SHORT);
+	}
+
+	buffer = buf;
+
+	for(i = 0; i < len; i++)
+	{
+		if(bst -> num.u & (1 << i))
+		{
+			buffer[i] = '1';
+		}
+		else
+		{
+			buffer[i] = '0';
+		}
+	}
+
+	return (BS_ERR_NONE);
+}
--- c/bitstring/bitstring.h
+++ c/bitstring/bitstring.h
@@ -1,15 +1,57 @@
 #ifndef __BITSTRING_H_
 #define __BITSTRING_H_
+
 #include <stdint.h>
+#include <stdlib.h>
 
-// These generate a K&R string with the bit representation of an input, num, and return a pointer to the string it produces.
-// They handle up to 64 bits.
-// They are not terminated with line endings.
-// The string represents how the number would appear in a register of the minimum size needed to store that number: padded with 0s from the beginning, or 1s for a negative number
+typedef enum
+{
+	BS_INIT_ME = 0,
+	BS_S8,
+	BS_S16,
+	BS_S32,
+	BS_S64,
+	BS_U8,
+	BS_U16,
+	BS_U32,
+	BS_U64,
+} bs_type_t;
 
-// Typically if num < 0 you want to use Signed, Unsigned will produce a 64 char string if num was negative.
+typedef union
+{
+	uint64_t u;
+	int64_t s;
+} bs_param_t;
 
-char* GenerateBitStringUnsigned(const uint64_t num);
-char* GenerateBitStringSigned(const int64_t num);
+typedef struct
+{
+	bs_param_t	num;
+	bs_type_t		type;
+} bs_t;
 
+typedef enum
+{
+	BS_ERR_NONE = 0,
+	BS_NULL_ARG,
+	BS_INVALID_ARG,
+	BS_ALLOC_FAIL,
+	BS_UNINITIALIZED,
+	BS_BUF_SHORT,
+} bs_error_t;
+
+bs_type_t bitstring_type_unsigned(uint64_t num);
+
+bs_type_t bitstring_type_signed(int64_t num);
+
+bs_error_t bitstring_reset(bs_t* bst);
+
+bs_error_t bitstring_create(bs_t* bst);
+
+bs_error_t bitstring_create_alloc(bs_t** bst);
+
+bs_error_t bitstring_make_signed(bs_t* bst, int64_t num);
+
+bs_error_t bitstring_make_unsigned(bs_t* bst, int64_t num);
+
+bs_error_t bitstring_serialize(bs_t* bst, void* buf, size_t buf_len);
 #endif
