64354e283ce22d6a6a0a2ea06a6c532632082ad4
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2017-04-07 10:01:34 -0500

    some explanation

--- c/bitstring/bitstring.h
+++ c/bitstring/bitstring.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+// Contains the possible types of a bitstring object
 typedef enum
 {
 	BS_INIT_ME = 0,
@@ -17,18 +18,21 @@ typedef enum
 	BS_U64,
 } bs_type_t;
 
+// The bitstring numerical object
 typedef union
 {
 	uint64_t u;
 	int64_t s;
 } bs_param_t;
 
+// The bitstring object
 typedef struct
 {
 	bs_param_t	num;
 	bs_type_t		type;
 } bs_t;
 
+// Possible return values for functions that can fail
 typedef enum
 {
 	BS_ERR_NONE = 0,
@@ -39,22 +43,34 @@ typedef enum
 	BS_BUF_SHORT,
 } bs_error_t;
 
-bs_type_t bitstring_type_unsigned(uint64_t num);
-
-bs_type_t bitstring_type_signed(int64_t num);
-
-bs_error_t bitstring_reset(bs_t* bst);
-
+/// UI
+// Create a bitstring object in the memory pointed to by bst
 bs_error_t bitstring_create(bs_t* bst);
 
+// Create a dynamically allocated bitstring object in the pointer-pointer bst
 bs_error_t bitstring_create_alloc(bs_t** bst);
 
+// Reset a bitstring object to the freshly created state
+bs_error_t bitstring_reset(bs_t* bst);
+
+// Pack a signed number into a bitstring
 bs_error_t bitstring_make_signed(bs_t* bst, int64_t num);
 
+// Pack an unsigned number into a bitstring
 bs_error_t bitstring_make_unsigned(bs_t* bst, int64_t num);
 
+// Serialize the bitstring as a string of bits into buf
+// Prefixed with an 's' for signed, 'u' for unsigned, null terminated
 bs_error_t bitstring_serialize(bs_t* bst, void* buf, size_t buf_len);
 
+// Destroys a dynamically allocated bitstring
 bs_error_t bitstring_destroy(bs_t* bst);
+
+
+
+// not UI
+bs_type_t bitstring_type_unsigned(uint64_t num);
+bs_type_t bitstring_type_signed(int64_t num);
+
 #endif
 
