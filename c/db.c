a7e438f66328638e8a68d257e0e9fdf60e8a3b6b
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2017-12-09 14:26:22 -0600

    read, write implemented on a bigger table with all types supported by sqlite3

--- c/db.c
+++ c/db.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <sqlite3.h>
 #include <assert.h>
+#include <sys/time.h>
+#include <math.h>
+#include <inttypes.h>
+#include <stdbool.h>
 
 const char* DB = "/media/sf_vboxshare/test.db";
-
 const char* TABLES[] = {
 	"test_table1",
 	"test_table2",
@@ -14,51 +17,71 @@ int main()
 	sqlite3* db = NULL;
 	sqlite3_stmt* stmt = NULL;
 	int ret = 0;
+	char _stmt[512] = { 0 };
+	struct timeval tv;
+	uint8_t array[] = {
+		0, 1, 2, 3, 4, 5, 6, 7,
+		7, 6, 5, 4, 3, 2, 1, 0,
+	};
 
-	ret = sqlite3_open_v2(
+	(void) sqlite3_open_v2(
 			DB,
 			&db,
 			SQLITE_OPEN_FULLMUTEX | SQLITE_OPEN_READWRITE,
-			NULL
-			);
-	//printf("open: %d\n", ret);
+			NULL);
 
-	sqlite3_exec(db, "BEGIN", NULL, NULL, NULL);
-	for(size_t j = 0; j < 2; j ++) {
 
-		char _stmt[128] = { 0 };
-		(void) snprintf(_stmt, 128, "INSERT INTO %s(value) VALUES (?)", TABLES[j]);
+	/* test writing data conforming to the schema */
+	snprintf(_stmt, 256, "INSERT INTO test_table1(record_ts_unix, integer, \
+		text, blob, real) VALUES (?, ?, ?, ?, ?)");
 
-		for(size_t i = 0; i < 100; i ++) {
-			ret = sqlite3_prepare_v2(db, _stmt, -1, &stmt, NULL);
-			if(SQLITE_OK != ret) {
-				printf("prepare: %d\n", ret);
-				break;
-			}
+	(void) sqlite3_exec(db, "BEGIN", NULL, NULL, NULL);
+	for(size_t i = 1; i < 10; i ++) {
+		(void) gettimeofday(&tv, NULL);
+		(void) sqlite3_prepare_v2(db, _stmt, -1, &stmt, NULL);
+		(void) sqlite3_bind_int(stmt, 1, tv.tv_sec);
+		(void) sqlite3_bind_int(stmt, 2, i);
+		(void) sqlite3_bind_text(stmt, 3, "ahoy, matey!", -1, SQLITE_STATIC);
+		(void) sqlite3_bind_blob(stmt, 4, array, 16, SQLITE_STATIC);
+		(void) sqlite3_bind_double(stmt, 5, pow(i, 3));
+		(void) sqlite3_step(stmt);
+		(void) sqlite3_finalize(stmt);
+	}
+	(void) sqlite3_exec(db, "COMMIT", NULL, NULL, NULL); // assume it all worked
 
-			ret = sqlite3_bind_int(stmt, 1, i);
-			if(SQLITE_OK != ret) {
-				printf("prepare: %d\n", ret);
-				break;
-			}
+	/* test reading data from a table*/
+	snprintf(_stmt, 256, "SELECT * FROM test_table1");
+	(void) sqlite3_prepare_v2(db, _stmt, -1, &stmt, NULL);
 
-			ret = sqlite3_step(stmt);
-			if(SQLITE_DONE != ret) {
-				printf("prepare: %d\n", ret);
-				break;
-			}
+	while(true) {
+		ret = sqlite3_step(stmt);
+		if(SQLITE_ROW != ret) { break; }
 
-			ret = sqlite3_finalize(stmt);
-			if(SQLITE_OK != ret) {
-				printf("prepare: %d\n", ret);
-				break;
-			}
+		int pkey = sqlite3_column_int(stmt, 0);
+		const unsigned char* insertion_ts = sqlite3_column_text(stmt, 1);
+		int timestamp = sqlite3_column_int(stmt, 2);
+		int integer = sqlite3_column_int(stmt, 3);
+		const unsigned char* text = sqlite3_column_text(stmt, 4);
+		size_t size = sqlite3_column_bytes(stmt, 5);
+		const uint8_t* blob = sqlite3_column_blob(stmt, 5);
+		double real = sqlite3_column_double(stmt, 6);
+
+		printf ("%d, %s, %d, %d, %s, %lf\n",
+				pkey,
+				insertion_ts,
+				timestamp,
+				integer,
+				text,
+				real);
+
+		for (size_t i = 0; i < size; i ++)
+		{
+			printf ("%.2" PRIX8 " ", blob[i]);
 		}
+		puts("\n");
 	}
+	sqlite3_finalize(stmt);
 
-	if(0 == ret) {
-		sqlite3_exec(db, "COMMIT", NULL, NULL, NULL);
-	}
 
 	(void) ret;		// hush the compiler
 	goto cleanup; // hush the compiler
