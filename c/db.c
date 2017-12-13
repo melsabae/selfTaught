136247e67a1f17941ad604461928509d90e4fb24
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2017-12-12 18:51:04 -0600

    reset instead of finalize

--- c/db.c
+++ c/db.c
@@ -36,52 +36,53 @@ int main()
 		text, blob, real) VALUES (?, ?, ?, ?, ?)");
 
 	(void) sqlite3_exec(db, "BEGIN", NULL, NULL, NULL);
-	for(size_t i = 1; i < 10; i ++) {
+	(void) sqlite3_prepare_v2(db, _stmt, -1, &stmt, NULL);
+	for(size_t i = 0; i < 1000000; i ++) {
 		(void) gettimeofday(&tv, NULL);
-		(void) sqlite3_prepare_v2(db, _stmt, -1, &stmt, NULL);
 		(void) sqlite3_bind_int(stmt, 1, tv.tv_sec);
 		(void) sqlite3_bind_int(stmt, 2, i);
 		(void) sqlite3_bind_text(stmt, 3, "ahoy, matey!", -1, SQLITE_STATIC);
 		(void) sqlite3_bind_blob(stmt, 4, array, 16, SQLITE_STATIC);
 		(void) sqlite3_bind_double(stmt, 5, pow(i, 3));
 		(void) sqlite3_step(stmt);
-		(void) sqlite3_finalize(stmt);
+		(void) sqlite3_reset(stmt);
 	}
+	(void) sqlite3_finalize(stmt);
 	(void) sqlite3_exec(db, "COMMIT", NULL, NULL, NULL); // assume it all worked
 
-	/* test reading data from a table*/
-	snprintf(_stmt, 256, "SELECT * FROM test_table1");
-	(void) sqlite3_prepare_v2(db, _stmt, -1, &stmt, NULL);
-
-	while(true) {
-		ret = sqlite3_step(stmt);
-		if(SQLITE_ROW != ret) { break; }
-
-		int pkey = sqlite3_column_int(stmt, 0);
-		const unsigned char* insertion_ts = sqlite3_column_text(stmt, 1);
-		int timestamp = sqlite3_column_int(stmt, 2);
-		int integer = sqlite3_column_int(stmt, 3);
-		const unsigned char* text = sqlite3_column_text(stmt, 4);
-		size_t size = sqlite3_column_bytes(stmt, 5);
-		const uint8_t* blob = sqlite3_column_blob(stmt, 5);
-		double real = sqlite3_column_double(stmt, 6);
-
-		printf ("%d, %s, %d, %d, %s, %lf\n",
-				pkey,
-				insertion_ts,
-				timestamp,
-				integer,
-				text,
-				real);
-
-		for (size_t i = 0; i < size; i ++)
-		{
-			printf ("%.2" PRIX8 " ", blob[i]);
-		}
-		puts("\n");
-	}
-	sqlite3_finalize(stmt);
-
+//	/* test reading data from a table*/
+//	snprintf(_stmt, 256, "SELECT * FROM test_table1");
+//	(void) sqlite3_prepare_v2(db, _stmt, -1, &stmt, NULL);
+//
+//	while(true) {
+//		ret = sqlite3_step(stmt);
+//		if(SQLITE_ROW != ret) { break; }
+//
+//		int pkey = sqlite3_column_int(stmt, 0);
+//		const unsigned char* insertion_ts = sqlite3_column_text(stmt, 1);
+//		int timestamp = sqlite3_column_int(stmt, 2);
+//		int integer = sqlite3_column_int(stmt, 3);
+//		const unsigned char* text = sqlite3_column_text(stmt, 4);
+//		size_t size = sqlite3_column_bytes(stmt, 5);
+//		const uint8_t* blob = sqlite3_column_blob(stmt, 5);
+//		double real = sqlite3_column_double(stmt, 6);
+//
+//		printf ("%d, %s, %d, %d, %s, %lf\n",
+//				pkey,
+//				insertion_ts,
+//				timestamp,
+//				integer,
+//				text,
+//				real);
+//
+//		for (size_t i = 0; i < size; i ++)
+//		{
+//			printf ("%.2" PRIX8 " ", blob[i]);
+//		}
+//		puts("\n");
+//	}
+//	sqlite3_finalize(stmt);
+//
 
 	(void) ret;		// hush the compiler
 	goto cleanup; // hush the compiler
