b1aa3d31b418306f3c19c39cfc6a2d508a320a43
mohammad el- \ sabae <mohammad.elsabae@gmail.com>
2018-05-01 00:28:04 -0500

    Update binary_to_uint.py

--- python/binary_to_uint.py
+++ python/binary_to_uint.py
@@ -1,4 +1,5 @@
-from functools import partial as pa
+from functools import partial as p
+from struct import unpack as u
 
 
 def h_uint(h: str, s: slice) -> int:
@@ -9,6 +10,14 @@ def b_uint(b: bytes, s: slice) -> int:
     return t(b[s])
 
 
-t = pa(int.from_bytes, byteorder='little')
-get_h8, get_h16, get_h32, get_h64 = map(lambda _: pa(h_uint, s=_), map(lambda _: slice(2 ** _), range(1, 5)))  # 2,4,8,16
-get_b8, get_b16, get_b32, get_b64 = map(lambda _: pa(b_uint, s=_), map(lambda _: slice(2 ** _), range(4)))  # 1,2,4,8
+def h_float(h: str) -> float:
+    return u("<f", bytes.fromhex(h))[0]
+
+
+def b_float(h: bytes) -> float:
+    return u("<f", h)[0]
+
+
+t = p(int.from_bytes, byteorder='little')
+get_h8, get_h16, get_h32, get_h64 = map(lambda _: p(h_uint, s=_), map(lambda _: slice(2 ** _), range(1, 5)))  # 2,4,8,16
+get_b8, get_b16, get_b32, get_b64 = map(lambda _: p(b_uint, s=_), map(lambda _: slice(2 ** _), range(4)))  # 1,2,4,8
