77ad6d3dca26f103d7c6e925aa3b3b23d7b7093d
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2023-01-19 11:21:50 -0500

    try host and device specific definitions

--- cuda/cmake/test.cu
+++ cuda/cmake/test.cu
@@ -9,6 +9,27 @@
 const std::size_t N = 1 << 20;
 
 
+__device__ // can only be called from within a kernel, not from the host
+void vec_inc(float* const c, const std::size_t n)
+{
+    for (std::size_t i = threadIdx.x + (blockIdx.x * blockDim.x); i < n; i += (blockDim.x * gridDim.x))
+    {
+        ++ c[i];
+    }
+}
+
+
+__host__ // explicitly disallow this to run on the GPU, cannot be launched nor called from kernels
+void vec_inc2(float* const c, const std::size_t n)
+{
+    for (std::size_t i = 0; i < n; ++ i)
+    {
+        ++ c[i];
+    }
+}
+
+
+
 __global__
 void vec_add(float* const c, const float* const a, const float* const b, const std::size_t n)
 {
@@ -16,16 +37,8 @@ void vec_add(float* const c, const float* const a, const float* const b, const s
     {
         c[i] = a[i] + b[i];
     }
-}
 
-
-__global__
-void vec_inc(float* const c, const std::size_t n)
-{
-    for (std::size_t i = threadIdx.x + (blockIdx.x * blockDim.x); i < n; i += (blockDim.x * gridDim.x))
-    {
-        ++ c[i];
-    }
+    vec_inc(c, n);
 }
 
 
@@ -89,8 +102,9 @@ int main(void)
     vec_add<<<grid_size, block_size, 0, stream>>>(d_c, d_a, d_b, N);
 
     // this is slower to execute since we can just +1 in the prior kernel
-    // this is more for fun
-    vec_inc<<<grid_size, block_size, 0, stream>>>(d_c, N);
+    // however we want it in a separate function to keep code clean
+    // so its not able to be launched like a kernel regularly can
+    //vec_inc<<<grid_size, block_size, 0, stream>>>(d_c, N);
 
     if (cudaSuccess != cudaMemcpyAsync(h_c.data(), d_c, N * sizeof(h_c[0]), cudaMemcpyDeviceToHost, stream))
     {
@@ -104,9 +118,12 @@ int main(void)
         return -__LINE__;
     }
 
-    for (std::size_t i = 0; i < N; ++ i)
+    // now add 1 to everything again, once h_c has been received from the GPU via d_c
+    vec_inc2(h_c.data(), h_c.size());
+
+    for (std::size_t i = 0; i < N; ++i)
     {
-        if (h_a[i] + h_b[i] + 1 != h_c[i])
+        if (h_a[i] + h_b[i] + 2 != h_c[i])
         {
             //std::cout << i << " " << h_c[i] << std::endl;
             ++ error_count;
