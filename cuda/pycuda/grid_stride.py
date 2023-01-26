1404e98e6c770a751b5ab8beb3fe3ddf47f3dab0
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2023-01-26 10:30:09 -0500

    the interface is inconsistent apparently, i can access n now. havent been able to figure out why the calculations come out the way they do, this works 100% in c++

--- cuda/pycuda/grid_stride.py
+++ cuda/pycuda/grid_stride.py
@@ -13,19 +13,15 @@ mod = SourceModule("""
     __global__
     void vec_inc(float* const c, const std::size_t n)
     {
-        //const std::size_t n = 1 << 3;
-
-        //for (std::size_t x = threadIdx.x + (blockIdx.x * blockDim.x); x < n; x += (blockDim.x * gridDim.x))
-        for (std::size_t x = 0; x < 8; ++ x)
+        for (std::size_t x = threadIdx.x + (blockIdx.x * blockDim.x); x < n; x += (blockDim.x * gridDim.x))
         {
-            ++c[n - 1];
-            //for (std::size_t y = threadIdx.y + (blockIdx.y * blockDim.y); y < n; y += (blockDim.y * gridDim.y))
-            //{
-            //    for (std::size_t z = threadIdx.z + (blockIdx.z * blockDim.z); z < n; z += (blockDim.z * gridDim.z))
-            //    {
-            //        c[(x * n * n) + (y * n) + z] = 4;
-            //    }
-            //}
+            for (std::size_t y = threadIdx.y + (blockIdx.y * blockDim.y); y < n; y += (blockDim.y * gridDim.y))
+            {
+                for (std::size_t z = threadIdx.z + (blockIdx.z * blockDim.z); z < n; z += (blockDim.z * gridDim.z))
+                {
+                    c[(x * n * n) + (y * n) + z] = 4;
+                }
+            }
         }
     }
 """)
@@ -34,9 +30,10 @@ do_the_thing = mod.get_function("vec_inc")
 
 a = numpy.arange(N, dtype=float)
 
+block=(16,1,1)
+grid=(1,1,1)
 #drv.In, drv.Out, drv.InOut
-do_the_thing(drv.InOut(a), drv.In(N), block=(16,8,8), grid=(1,1,1))
-#do_the_thing(drv.InOut(a), block=(16,1,1), grid=(1,1,1))
+do_the_thing(drv.InOut(a), N, block=block, grid=grid)
 
 max_elem = numpy.argmax(a)
 print(max_elem, a[max_elem])
