56a7a815a7567575cc87ddecde7572e9d247b63b
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2023-02-16 16:09:59 -0500

    update the file content dynamically

--- file_passing/producer.c
+++ file_passing/producer.c
@@ -9,22 +9,28 @@
 #include <signal.h>
 
 
+const size_t file_size = 255;
 const char* const file_name = "test";
 const int file_flags = O_RDWR | O_CREAT | O_EXCL;
 const mode_t file_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-const char* test_data = "ahoy, matey!";
 
 struct
 {
     bool signaled;
     sigset_t signals;
     struct sigaction signal_action;
+    void* buffer;
 }
 program_state;
 
 
 void at_exit(void)
 {
+    if (0 != munmap(program_state.buffer, file_size))
+    {
+        fprintf(stderr, "failed to munmap\n");
+    }
+
     if (0 != shm_unlink(file_name))
     {
         fprintf(stderr, "failed to unlink shm file\n");
@@ -32,7 +38,7 @@ void at_exit(void)
 }
 
 
-void catch_stop(int signal)
+void signal_handler(int signal)
 {
     program_state.signaled = true;
     (void) signal;
@@ -54,7 +60,7 @@ int main(int argc, char** argv)
     }
 
     program_state.signaled = false;
-    program_state.signal_action.sa_handler = catch_stop;
+    program_state.signal_action.sa_handler = signal_handler;
     program_state.signal_action.sa_mask = program_state.signals;
     program_state.signal_action.sa_flags = 0;
 
@@ -64,6 +70,7 @@ int main(int argc, char** argv)
         return -__LINE__;
     }
 
+    // create a file path to share memory with
     const int fd = shm_open(file_name, file_flags, file_mode);
 
     if (0 > fd)
@@ -72,22 +79,28 @@ int main(int argc, char** argv)
         return -__LINE__;
     }
 
-    if (0 != ftruncate(fd, strlen(test_data) + 1))
+    // resize the file path for the expected size of the input data
+    if (0 != ftruncate(fd, file_size))
     {
-        fprintf(stderr, "failed to ftruncate shm file\n");
+        fprintf(stderr, "failed to ftruncate file\n");
         return -__LINE__;
     }
 
-    if (strlen(test_data) != write(fd, test_data, strlen(test_data)))
+    // create a virtual mapping so the file contents can be dealt with as a pointer
+    program_state.buffer = mmap(NULL, file_size, PROT_WRITE, MAP_SHARED, fd, 0);
+
+    if (NULL == program_state.buffer)
     {
-        fprintf(stderr, "failed to write to shm file\n");
+        fprintf(stderr, "failed to mmap file\n");
         return -__LINE__;
     }
 
+    size_t i = 0;
     while(false == program_state.signaled)
     {
-        //fprintf(stderr, "pause returned %d\n", pause());
-        sigsuspend(NULL);
+        snprintf(program_state.buffer, file_size, "%s %d", "ahoy matey!", i);
+        ++ i;
+        usleep(1E3);
     }
 
     return 0;
