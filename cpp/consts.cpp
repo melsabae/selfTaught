7e79ddf405e6e897514956bcbfdb544558333bb7
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2023-03-11 13:34:14 -0500

    .

--- cpp/consts.cpp
+++ cpp/consts.cpp
@@ -17,6 +17,14 @@ consteval double sol_scaled2(const double n)
     return sol2 * n;
 }
 
+constexpr void mut_x(const int& x)
+{
+    // const is just a compiler hint, if the storage is not in ROM, then you can mutate it
+    // this will segfault if x is actually non-mutable
+    auto& non_const_x = const_cast<int&>(x);
+    non_const_x = x + 5;
+}
+
 int main(int argc, char** argv)
 {
     //auto& non_const_sol = const_cast<double&>(sol);
@@ -39,6 +47,12 @@ int main(int argc, char** argv)
     std::cout << print_sol2 << std::endl;
     std::cout << print_scaled_sol << std::endl;
     std::cout << print_scaled_sol2 << std::endl;
+
+
+    int x = 77;
+    std::cout << x << std::endl;
+    mut_x(x);
+    std::cout << x << std::endl;
 
     return 0;
 }
